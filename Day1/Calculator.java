bd5a26f8fbab57cbb3b3b89df0f8547799935fc5
arokiabhavy <arokia.bhavya@gmail.com>
2021-06-05 13:26:36 +0530

    Day1 assignment

---
+++ Day1/Calculator.java
@@ -0,0 +1,11 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		System.out.println("Addition:"+(Integer.parseInt(args[0])+Integer.parseInt(args[1])));
+		System.out.println("Subtraction:"+(Integer.parseInt(args[0])-Integer.parseInt(args[1])));
+		System.out.println("Multiplication:"+(Integer.parseInt(args[0])*Integer.parseInt(args[1])));
+		System.out.println("Division:"+(Integer.parseInt(args[0])/Integer.parseInt(args[1])));
+	}
+
+}
