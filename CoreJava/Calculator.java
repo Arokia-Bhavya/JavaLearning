a9e9be5ff7afeb8e119313111127f49a31738e2c
arokiabhavy <arokia.bhavya@gmail.com>
2021-06-06 11:14:13 +0530

    CoreJava

--- CoreJava/Calculator.java
+++ CoreJava/Calculator.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 
 public class Calculator {
 
 	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println(scan.nextInt());
+		System.out.println(scan.nextInt());
 		System.out.println("Addition:"+(Integer.parseInt(args[0])+Integer.parseInt(args[1])));
 		System.out.println("Subtraction:"+(Integer.parseInt(args[0])-Integer.parseInt(args[1])));
 		System.out.println("Multiplication:"+(Integer.parseInt(args[0])*Integer.parseInt(args[1])));
