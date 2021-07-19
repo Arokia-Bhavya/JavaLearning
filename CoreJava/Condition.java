fe8a5f33ede565068d86a884b069f0e506b1432d
Arokia-Bhavya <arokia.bhavya@gmail.com>
2021-07-19 09:01:15 +0530

    condition

---
+++ CoreJava/Condition.java
@@ -0,0 +1,14 @@
+
+public class Condition {
+
+	public static void main(String[] args) {
+		int i=1;
+		int j=1;
+		if(i > j)
+			System.out.println("inside if");
+		else
+			System.out.println("inside else");
+
+	}
+
+}
